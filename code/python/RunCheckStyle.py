a61384b27382eebee2d734add22ec711ab797427
idanacumen <idan@acumen.io>
2021-04-18 09:24:36 +0300

    Updated run

--- code/python/RunCheckStyle.py
+++ code/python/RunCheckStyle.py
@@ -3,15 +3,17 @@ import os
 
 repos_path = '~/src/'
 output_path = '/tmp/checkstyle_output/'
-checkstyle_jar = '~/Downloads/checkstyle-8.23-all.jar' # original version
+#checkstyle_jar = '~/Downloads/checkstyle-8.23-all.jar' # original version
+checkstyle_jar = '~/Downloads/checkstyle-8.41.1-all.jar' # new version
 config='~/src/follow-your-nose/code/config/follow_your_nose_checks.xml'
 
 checkstyle_cmd_template = \
-    'java -cp {jar} com.puppycrawl.tools.checkstyle.Main -c {config} {repos_path} > {output_path}{repo}.txt '
+    'java -cp {jar} com.puppycrawl.tools.checkstyle.Main -c {config} {repos_path}{repo} > {output_path}{repo}.txt '
 
 err_dirs = []
 #repos = os.listdir(path)
-repos = ['batfish']
+repos = ['checkstyle' #, 'batfish'
+ ]
 for repo in repos:
     try:
         repo_path = os.path.join(repos_path, repo)
